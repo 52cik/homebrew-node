c362604bb81cff19560e924f17bfb1939121d614
楼教主 <cik520@qq.com>
2018-11-16 10:06:57 +0800

    update v11.2.0

--- Casks/node11.rb
+++ Casks/node11.rb
@@ -1,6 +1,6 @@
 cask 'node11' do
-  version '11.0.0'
-  sha256 '44b57ab4b4d64168129c53c5053b01e75177bc53699853a9907ee22020c510af'
+  version '11.2.0'
+  sha256 'e5fc79d4a840913f53b6ff6be66f6c2eec23a9ebe2c606bb690b32d984226ffc'
 
   # https://npm.taobao.org/mirrors/node/latest-v11.x/SHASUMS256.txt
 

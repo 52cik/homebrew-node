cb5425396b2ecf990e50d32b60cf111b04c5a9da
楼教主 <cik520@qq.com>
2018-12-05 15:51:20 +0800

    update v6.15.1

--- Casks/node6.rb
+++ Casks/node6.rb
@@ -1,6 +1,6 @@
 cask 'node6' do
-  version '6.15.0'
-  sha256 '2c747672da6bad59e37be1f23eed7175786538870e4bf6298e59fe6e3097d2c0'
+  version '6.15.1'
+  sha256 '4e3675e929506a2ec05f232cb220995ac2c31f7e8c6e2d6b46dabffaedc51075'
 
   # https://npm.taobao.org/mirrors/node/latest-v6.x/SHASUMS256.txt
 

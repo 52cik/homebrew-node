5eec550522afc9e2d3c48dca483e6764407b77cb
楼教主 <cik520@qq.com>
2019-08-08 00:02:32 +0800

    v6.17.1

---
+++ Casks/node@6.rb
@@ -0,0 +1,27 @@
+cask 'node@6' do
+  version '6.17.1'
+  sha256 '372ddb024919f0ee9293b222adcb284c5ad56e914b8f844f4d4bd236f42f39d0'
+  # https://npm.taobao.org/mirrors/node/latest-v6.x/SHASUMS256.txt
+
+  url "https://cdn.npm.taobao.org/dist/node/v#{version}/node-v#{version}.pkg"
+  name 'Node.js'
+  homepage 'https://nodejs.org/'
+
+  pkg "node-v#{version}.pkg"
+
+  uninstall pkgutil: [
+                       'org.nodejs.node.pkg',
+                       'org.nodejs.npm.pkg'
+                     ]
+
+  zap delete: [
+               '/usr/local/lib/node_modules',
+               '~/.npmrc',
+               '~/.npm',
+               '~/.node-gyp',
+               '~/.node_repl_history',
+               '~/.v8flags*',
+               '~/.yarnrc',
+               '~/Library/Caches/Yarn'
+              ]
+end
